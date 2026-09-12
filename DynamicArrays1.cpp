80c1297425555287f481f8c134d0dd1fd436ce33
Eric-Giulio Hedes <literally-bugsie@Eric-Giulios-MacBook-Air.local>
2026-09-12 00:41:36 -0700

    Whoops, here's the real commit

---
+++ DynamicArrays1.cpp
@@ -0,0 +1,13 @@
+//COMSC-210 | Lab 6 | Eric-Giulio Hedes
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+double enterArrayData();
+void outputArrayData();
+void sumArray();
+
+int main()
+{
+    
+}
