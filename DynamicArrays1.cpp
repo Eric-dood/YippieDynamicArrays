cf7376e5c893cd77c8a7d3f100d275dcdc4e3d0d
Eric-Giulio Hedes <literally-bugsie@Eric-Giulios-MacBook-Air.local>
2026-09-12 17:10:17 -0700

    Slowly shaping up the enterArrayData() function

--- DynamicArrays1.cpp
+++ DynamicArrays1.cpp
@@ -3,11 +3,25 @@
 #include <iomanip>
 using namespace std;
 
-double enterArrayData();
+const int SIZE = 5;
+
+void enterArrayData(double*);
 void outputArrayData();
 void sumArray();
 
 int main()
 {
-    
+    double *arrayPtr = nullptr;
+    enterArrayData(arrayPtr);
+}
+
+void enterArrayData(double *arr)
+{
+    cout << "Data entry for the array:" << endl;
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << "\t Element #" << i << ": ";
+        cin >> *(arr + i);
+        cin.ignore(1000, 10);
+    }
 }
