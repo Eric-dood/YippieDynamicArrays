e0ee663d2d41e5ddbf7f5b416940dd71422fec2a
Eric-Giulio Hedes <literally-bugsie@Eric-Giulios-MacBook-Air.local>
2026-09-12 17:25:38 -0700

    Still working on outputArrayData() + added outputArrayData() and sumArray()

--- DynamicArrays1.cpp
+++ DynamicArrays1.cpp
@@ -5,23 +5,41 @@ using namespace std;
 
 const int SIZE = 5;
 
-void enterArrayData(double*);
-void outputArrayData();
-void sumArray();
+double * enterArrayData();
+void outputArrayData(double*);
+double sumArray(double*);
 
 int main()
 {
     double *arrayPtr = nullptr;
-    enterArrayData(arrayPtr);
+    arrayPtr = enterArrayData();
+    outputArrayData(arrayPtr);
 }
 
-void enterArrayData(double *arr)
+double * enterArrayData()
 {
+    double *temp = nullptr;
     cout << "Data entry for the array:" << endl;
     for (int i = 0; i < SIZE; i++)
     {
         cout << "\t Element #" << i << ": ";
-        cin >> *(arr + i);
+        cin >> *(temp + i);
         cin.ignore(1000, 10);
     }
+    return temp;
+}
+
+void outputArrayData(double *arr)
+{
+    cout << "Outputting data array elements: ";
+    for (int i = 0; i < SIZE; i++)
+        cout << *(arr + i) << " ";
+}
+
+double sumArray(double *arr)
+{
+    double sum;
+    for (int i = 0; i < SIZE; i++)
+        sum += *(arr + i);
+    return sum;
 }
